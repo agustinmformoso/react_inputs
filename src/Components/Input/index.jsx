6d08c7bc53f719a38f593096af945ff13b613605
Agustin Formoso <agustinf@lagash.com>
2019-09-13 11:31:47 -0300

    Componentes armados

---
+++ src/Components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./styles.css";
+
+const Input = ({ type }) => {
+    return (
+            <input type={type} />
+    )
+}
+
+export default Input;
--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -5,11 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Input from '../../Components/Input';
 
 const Home = () => {
     return (
        <Container fluid={true} className="Home">
-           <p>donna</p>
+           <Input type="email" />
        </Container>
     )
 }
