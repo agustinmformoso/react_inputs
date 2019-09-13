e925a8e9d2589acfaeeda8ed1e50fda103681701
Agustin Formoso <agustinf@lagash.com>
2019-09-13 10:40:44 -0300

    Initial commit

---
+++ src/Containers/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+    return (
+       <h1>Inputs</h1>
+    )
+}
+
+export default Home;
