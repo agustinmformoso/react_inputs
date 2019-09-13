82c0e479bff3fe9eb731d3ddfd47a07fd20d062b
Agustin Formoso <agustinf@lagash.com>
2019-09-13 12:20:06 -0300

    Creo configuracion de los botones

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import "./styles.css";
-<<<<<<< HEAD
-=======
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
->>>>>>> 9968b69aa6668503965c55ed6325c6e2ad8c7065
 
 const Header = () => {
     return (
