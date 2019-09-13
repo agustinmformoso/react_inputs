e82feb65a3647c2a5f7101b7b74f773ea00d859f
Agustin Formoso <agustinf@lagash.com>
2019-09-13 11:07:04 -0300

    Normalize agregado

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./styles.css";
+import "../../Resources/normalize.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Home = () => {
     return (
-       <h1>Inputs</h1>
+       <Container fluid={true} className="Home">
+           <p>donna</p>
+       </Container>
     )
 }
 
